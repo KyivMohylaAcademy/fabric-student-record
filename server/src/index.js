94fa972b5a11e8dcd90c22d3322ce6c4b0e16b44
Ivan Ishchenko <45439032+larderino1@users.noreply.github.com>
2021-04-02 21:39:30 +0300

    add ui + handle errors

--- server/src/index.js
+++ server/src/index.js
@@ -1,14 +1,29 @@
 import express from 'express'
-// import './models';
-// import { auth } from './routes';
+import { auth } from './routes';
 
 const app = express();
+app.set('view engine', 'ejs');
 app.use(express.urlencoded());
-
+app.use(express.static('public'));
 app.use(express.json());
-// app.use('/api/v1/', auth);
+
+app.use('/api/v1/', auth);
+
 const appPort = 3000;
+
 app.listen(
     appPort,
     () => console.log(`Listening on port ${appPort}...`),
 );
+
+app.get('/v1/', (req, res) => {
+    res.render('index')
+});
+
+app.get('/v1/register/student', (req, res) => {
+    res.render('student-register-view')
+});
+
+app.get('/v1/register/teacher', (req, res) => {
+    res.render('teacher-register-view')
+});
