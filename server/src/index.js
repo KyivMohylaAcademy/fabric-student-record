28d6bb78bda04a458b5b438108230f4d88242412
Dmitrii-Kh <actibagamma@gmail.com>
2021-04-02 12:31:35 -0400

    teacher enrollment + error handling + ui

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,15 @@
 import express from 'express'
-// import './models';
-// import { auth } from './routes';
-
+//import './models';
+import { auth } from './routes';
+const path = require('path');
 const app = express();
 app.use(express.urlencoded());
-
+app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.json());
-// app.use('/api/v1/', auth);
+app.use('/api/v1/', auth);
+app.get('/main', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'index.html'));
+});
 const appPort = 3000;
 app.listen(
     appPort,
--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,15 +1,64 @@
 import express from 'express';
 import FabricCAServices from 'fabric-ca-client';
+import {Gateway, InMemoryWallet, X509WalletMixin} from 'fabric-network';
 import fs from 'fs';
 import path from 'path';
 import yaml from 'js-yaml';
 
 const router = express.Router();
-const studentRegistration = async (req, res) => {
-  let caInfo = yaml.safeLoad(fs.readFileSync('../gateway/fabric-ca-client-config.yaml', 'utf8'));
-  const caTLSCACerts = caInfo.tlsCACerts.pem;
-  const ca = new FabricCAServices(caInfo.url, { trustedRoots: caTLSCACerts, verify: false }, caInfo.caName);
+const registration = async (req, res) => {
+    const {login, password, entity} = req.body;
+    
+    const ca = new FabricCAServices('http://0.0.0.0:7054');
+
+    try {
+    const adminData = await ca.enroll({enrollmentID: 'admin', enrollmentSecret: 'password'});
+
+    const identity = {
+        label: 'client',
+        certificate: adminData.certificate,
+        privateKey: adminData.key.toBytes(),
+        mspId: 'NAUKMA'
+    }
+    const wallet = new InMemoryWallet();
+    const mixin = X509WalletMixin.createIdentity(identity.mspId, identity.certificate, identity.privateKey);
+    await wallet.import(identity.label, mixin);
+    const gateway = new Gateway();
+    const connectionProfile = yaml.safeLoad(fs.readFileSync(path.resolve(__dirname, '../gateway/networkConnection.yaml'), 'utf8'));
+    const connectionOptions = {
+        identity: identity.label,
+        wallet: wallet,
+        discovery: {enabled: false, asLocalhost: true}
+    }
+    await gateway.connect(connectionProfile, connectionOptions);
+    const admin = gateway.getCurrentIdentity();
+    
+    await ca.register({
+        enrollmentID: login,
+        enrollmentSecret: password,
+        role: 'peer',
+        affiliation: `naukma.${entity}`,
+        maxEnrollments: -1
+    }, admin);
+
+    
+    const userData = await ca.enroll({enrollmentID: login, enrollmentSecret: password});
+  
+    gateway.disconnect();
+   
+    res.status(201).json({
+        login: login,
+        certificate: userData.certificate,
+        privateKey: userData.key.toBytes()
+    })
+    
+    console.log(userData.certificate + '\n' + userData.key.toBytes())
+  
+    } catch(e){
+        console.log("**** Enrollment error ****")
+        res.status(400).json({message : "Enrollment error", error : e.message}) 
+    }
 };
-router.post('/student', studentRegistration);
+router.post('/register', registration);
 
 export default router;
