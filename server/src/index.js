2281ac4d82d955894f75741b7b5ff0a8e4715189
Ferolka <andry.kovalenko@gmail.com>
2021-04-03 12:13:14 +0300

    register

--- server/src/index.js
+++ server/src/index.js
@@ -1,14 +1,21 @@
 import express from 'express'
-// import './models';
-// import { auth } from './routes';
-
+import { auth } from './routes';
+const path = require('path');
 const app = express();
 app.use(express.urlencoded());
 
+app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.json());
 // app.use('/api/v1/', auth);
+app.use('/api/v1/', auth);
+app.get('/home', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'index.html'));
+});
+
 const appPort = 3000;
+
 app.listen(
     appPort,
     () => console.log(`Listening on port ${appPort}...`),
 );
+
