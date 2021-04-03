e32de9bf761aaccafee834cebed76f3984b8ee06
anvin23 <andrey1vnnik@gmail.com>
2021-04-03 14:28:48 +0300

    Vynnyk (ui + error handling + teacher enroll)

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,1 +1,17 @@
-export { default as auth } from './auth';
+import express from 'express'
+//import './models';
+import { auth } from './routes';
+const path = require('path');
+const app = express();
+app.use(express.urlencoded());
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.json());
+app.use('/api/v1/', auth);
+app.get('/main', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'index.html'));
+});
+const appPort = 3000;
+app.listen(
+    appPort,
+    () => console.log(`Listening on port ${appPort}...`),
+);
