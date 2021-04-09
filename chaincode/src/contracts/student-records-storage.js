dfea1f656203c3c58bdf7d03525ca37f8cd6cd26
Michael Kobelev <misha.kobelev.99@gmail.com>
2021-04-09 10:51:35 +0000

    Additional functions

--- chaincode/src/contracts/student-records-storage.js
+++ chaincode/src/contracts/student-records-storage.js
@@ -8,9 +8,91 @@ class StudentRecordsStorage extends Contract {
     super('org.fabric.studentRecordsStorage');
   }
 
-  async createStudentRecord(ctx) {
+  async getStudentRecord(ctx, studentEmail) {
     const identity = new ClientIdentity(ctx.stub);
+    if (identity.cert.subject.organizationalUnitName !== 'admin') {
+      throw new Error("Current user does not have access to this function!");
+    }
 
+    const recordAsBytes = await ctx.stub.getState(studentEmail);
+    if (!recordAsBytes || recordAsBytes.toString().length === 0) {
+      throw new Error("Student with this email does not exist!");
+    }
+    return JSON.parse(recordAsBytes.toString());
+  }
+
+  async updateStudentRecord(ctx, studentEmail, record) {
+    const newRecordInBytes = Buffer.from(JSON.stringify(record));
+    await ctx.stub.putState(studentEmail, newRecordInBytes);
+  }
+
+  async createStudentRecord(ctx, studentEmail, fullName) {
+    const identity = new ClientIdentity(ctx.stub);
+    if (identity.cert.subject.organizationalUnitName !== 'admin') {
+      throw new Error("Current user does not have access to this function.");
+    }
+
+    const recordAsBytes = await ctx.stub.getState(studentEmail);
+    if (recordAsBytes && recordAsBytes.toString().length !== 0) {
+      throw new Error("Student with this email already exists!");
+    }
+
+    const record = {
+      fullName: fullName,
+      semesters: []
+    }
+
+    const newRecordInBytes = Buffer.from(JSON.stringify(record));
+    await ctx.stub.putState(studentEmail, newRecordInBytes);
+    return JSON.stringify(record, null, 2);
+  }
+
+
+  async addSubjectToStudentRecord(ctx, studentEmail, semesterNumber, subjectName) {
+    const record = await getStudentRecord(ctx, studentEmail);
+    const teacherEmail = identity.cert.subject.commonName;
+    
+    if (!record.semesters[semesterNumber]) {
+      record.semesters[semesterNumber] = {};
+    }
+
+    record.semesters[semesterNumber][subjectName] = {
+      lector: teacherEmail,
+      themes: []
+    }
+
+    await updateStudentRecord(ctx, studentEmail, record);
+    return JSON.stringify(record, null, 2);
+  }
+
+  async addGradeToStudentRecord(ctx, studentEmail, semester, subjectName, theme, grade) {
+    const record = await getStudentRecord(ctx, studentEmail);
+    if (!record.semesters[semester]?.[subjectName]) {
+      throw new Error("This subject in this semester does not exist!");
+    }
+
+    record.semesters[semester][subjectName].themes.push([
+      {
+        title: theme,
+        rating: grade,
+        date: Date.now()
+      }
+    ]);
+
+    await updateStudentRecord(ctx, studentEmail, record);
+    return JSON.stringify(record, null, 2);
+  }
+
+  async getAllStudentGrages(ctx, studentEmail) {
+    const record = await getStudentRecord(ctx, studentEmail);
+
+    return JSON.stringify(record.semesters, null, 2);
+  }
+
+  async getStudentGragesBySemester(ctx, studentEmail, semester) {
+    const record = await getStudentRecord(ctx, studentEmail);
+
+    return JSON.stringify(record.semesters[semester] || [], null, 2);
   }
 }
 
