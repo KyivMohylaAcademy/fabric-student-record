30fa49efd976cb0701c689ef7a029d44117f2ebf
Marina Kharchenko <jkh939443@gmail.com>
2021-04-24 08:55:49 +0300

    lab 5

--- server/src/index.js
+++ server/src/index.js
@@ -1,16 +1,17 @@
 import express from 'express'
-// import './models';
- import { auth } from './routes';
+import { auth } from './routes';
 
 const app = express();
-app.use(express.urlencoded());
 
+app.use(express.urlencoded());
 app.use(express.json());
- app.use('/api/v1/', auth);
-app.get('/api/v1/', (req, res) => res.render('home'));
-app.get('/api/v1/signup-teacher', (req, res) => res.render('signup-teacher'));
-const appPort = 3000;
-app.listen(
-    appPort,
-    () => console.log(`Listening on port ${appPort}...`),
-);
+
+
+app.use('/api/v1/', auth);
+
+const port = 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
+
+
+
+
